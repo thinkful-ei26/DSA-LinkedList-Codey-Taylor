9606d63a85340ff6fc7b61518701b2590d163b95
CodeyWorley <codeyworley@gmail.com>
2019-01-17 09:48:00 -0700

    init

---
+++ linked-lists.js
@@ -0,0 +1,95 @@
+class _Node {
+    constructor(value, next) {
+        this.value=value;
+        this.next=next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+            this.head = null;
+    }
+
+    insertFirst(item){
+        this.head = new _Node(item, this.head);
+    }
+
+    insertLast(item){
+        if(this.head === null){
+            this.insertFirst(item);
+        }
+        else{
+            let tempNode = this.head;
+            while(tempNode.next !== null){
+                tempNode = tempNode.next;
+            }
+            tempNode.next = new _Node(item, null);
+        }
+    }
+
+    find(item) { 
+        //start at the head
+        let currNode = this.head;
+        //if the list is empty
+        if (!this.head){
+            return null;
+        }
+        //Check for the item 
+        while(currNode.value !== item) {
+            //return null if end of the list 
+            // and the item is not on the list
+            if (currNode.next === null) {
+                return null;
+            }
+            else {
+                //otherwise keep looking 
+                currNode = currNode.next;
+            }
+        }
+        //found it
+        return currNode;
+    }
+
+    remove(item){ 
+        //if the list is empty
+        if (!this.head){
+            return null;
+        }
+        //if the node to be removed is head, make the next node head
+        if(this.head.value === item){
+            this.head = this.head.next;
+            return;
+        }
+        //start at the head
+        let currNode = this.head;
+        //keep track of previous
+        let previousNode = this.head;
+
+        while ((currNode !== null) && (currNode.value !== item)) {
+            //save the previous node 
+            previousNode = currNode;
+            currNode = currNode.next;
+        }
+        if(currNode === null){
+            console.log('Item not found');
+            return;
+        }
+        previousNode.next = currNode.next;
+    }
+}
+
+const items = ['Apollo', 'Boomer', 'Helo', 'Husker', 'Starbuck'];
+const main = items => {
+    const SLL = new LinkedList;
+    items.map(item => SLL.insertLast(item))
+
+    SLL.insertFirst('Tauhida');
+    console.log(SLL.find('Tauhida'));
+
+    SLL.remove('squirrel');
+
+    SLL.remove('Boomer');
+
+    return SLL;
+}
+console.log(main(items));
